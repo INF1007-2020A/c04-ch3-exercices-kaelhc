f22c69ffea995a43bb06553293e8dff37abf162e
kaelhc <karim.el-hosni@polymtl.ca>
2020-09-14 22:33:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-     angle
+     angle_radian=
     return 0.0
 
 
