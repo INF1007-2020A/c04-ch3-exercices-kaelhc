ad94af2f2ff95b644b60ae7d76b84c7634e0378f
kaelhc <karim.el-hosni@polymtl.ca>
2020-09-14 23:22:49 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    degre=
+    degre=angle_rads
 
     return 0.0, 0.0, 0.0
 
